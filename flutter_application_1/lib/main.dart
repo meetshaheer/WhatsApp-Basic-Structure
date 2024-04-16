0342e5a2765cd4aa39192c47b5bc0e55a7e3ef6c
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-04-16 15:47:01 +0500

    just creating main method and Stateless Class

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
