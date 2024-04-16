826c5bcf02ffaefd621e321e94615a8cf3543326
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-04-16 16:20:33 +0500

    designing archieve widget with icon and set the position

--- flutter_application_1/lib/home_view.dart
+++ flutter_application_1/lib/home_view.dart
@@ -6,25 +6,51 @@ class home_view extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("WhatsApp"),
-        foregroundColor: Colors.white,
-        backgroundColor: Colors.teal[700],
-        actions: const [
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(right: 20),
-                child: Icon(Icons.photo_camera),
+        appBar: AppBar(
+          title: const Text("WhatsApp"),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.teal[700],
+          actions: const [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: Icon(Icons.photo_camera),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Icon(Icons.more_vert),
+                )
+              ],
+            )
+          ],
+        ),
+        body: const Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 25.0),
+                    child: Icon(
+                      Icons.move_to_inbox,
+                      size: 22,
+                      color: Colors.teal,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 25, top: 3),
+                    child: Text(
+                      "Archived",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                  )
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.only(right: 10),
-                child: Icon(Icons.more_vert),
-              )
-            ],
-          )
-        ],
-      ),
-    );
+            )
+          ],
+        ));
   }
 }
